7713091a7166f5070e1298a5352f546dac8cf6dc
a12k <aaron@a12k.io>
2017-07-04 15:26:49 -0400

    some updates

--- routes/api.js
+++ routes/api.js
@@ -4,18 +4,21 @@ const search = require('../lib/search.js');
 
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to the api!' });
+    console.log("'hooray! welcome to the api!'");
 });
 
 router.post('/:uid', function(req, res, next) {
   search(req.params.uid).then(function(json){
     res.json({
       title: 'Ultra Tracker', 
+      raceid: json.uid,
       racetitle: json.racetitle, 
       spots: json.spots, 
       closes: json.closes, 
       location: json.location,
       date: json.eventdate
     });
+    console.log(json);
   }, function(error) {
     res.json({ title: 'ERROR' });
   })
